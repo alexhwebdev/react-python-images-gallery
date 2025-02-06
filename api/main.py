5c73e946ecb37a2fe14e1d99d69fc8589a8e13d2
Alex <alexhwebdev@gmail.com>
2025-02-06 15:41:02 -0800

    GET, POST image endpoints added

--- api/main.py
+++ api/main.py
@@ -1,8 +1,12 @@
 import os                         # Used for .env.local
 import requests                   # Allows Requests is a simple, yet elegant, HTTP library.
-from flask import Flask, request  # In Flask, we can access query params using request obj
+from flask import Flask, json, request, jsonify  # In Flask, we can access query params using request obj
 from dotenv import load_dotenv    #
 from flask_cors import CORS       # https://pypi.org/project/Flask-Cors/ : Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery      # db = gallery
+images_collection = gallery.images  # collection = images
 
 load_dotenv(dotenv_path="./.env.local")
 # print("os.environ ", os.environ)  # UNSPLASH_KEY should be visible with load_dotenv and relative path added
@@ -19,6 +23,8 @@ CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+# insert_test_document()
+
 @app.route("/new-image")            # Decorator function : route - registers function
 def new_image():
   word = request.args.get("query")  # query : ?query=car
@@ -33,12 +39,35 @@ def new_image():
     headers=headers, 
     params=params
   )
-
   # print(response.json())
   # return {"word": word}
   data = response.json()
   return {"data": data}  # if you return dictionary, 'content-type' will be JSON
   # return data
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+  if request.method == "GET":
+    # read images from the database
+    images = images_collection.find({})
+    return jsonify([img for img in images])
+  if request.method == "POST":
+    # save image in the database
+    # json.loads(request.data)
+    image = request.get_json()
+    # print("image ", image)                # image  {'id': '123', 'title': 'My images'}
+    image["_id"] = image.get("id")
+    # print("image['_id'] ", image["_id"])  # image['_id']  123
+
+    result = images_collection.insert_one(image)
+    print("result ", result)
+    inserted_id = result.inserted_id
+    print("inserted_id ", inserted_id)
+    return {"inserted_id": inserted_id}
+    # return {
+    #   "result": result,
+    #   "inserted_id": inserted_id
+    # }
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5050)
