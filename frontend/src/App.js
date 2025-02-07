57d1a5c3c7d0a31dc0632697762be036f662b031
Alex <alexhwebdev@gmail.com>
2025-02-06 16:02:34 -0800

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
 import Welcome from './components/Welcome';
+import Spinner from './components/Spinner';
 import { Container, Row, Col } from 'react-bootstrap';
 
 // const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
@@ -13,49 +17,107 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const handleSearchSubmit = (e) => {
+  const getSavedImages = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/images`);
+      setImages(res.data || []);
+      setLoading(false);
+      toast.success('Saved images downloaded');
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => getSavedImages(), []);
+
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    console.log('word ', word);
-    fetch(
-      // `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
-      `${API_URL}/new-image?query=${word}`,
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log('data ', data);
-        setImages([{ ...data, title: word }, ...images]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+    try {
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`);
+      setImages([{ ...res.data, title: word }, ...images]);
+      toast.info(`New image ${word.toUpperCase()} was found`);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
 
     setWord('');
   };
 
-  const handleDeleteImage = (id) => {
-    setImages(images.filter((image) => image.data.id !== id));
+  const handleDeleteImage = async (id) => {
+    try {
+      const res = await axios.delete(`${API_URL}/images/${id}`);
+      if (res.data?.deleted_id) {
+        toast.warn(
+          `Image ${images
+            .find((i) => i.id === id)
+            .title.toUpperCase()} was deleted`,
+        );
+        setImages(images.filter((image) => image.id !== id));
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
 
+  const handleSaveImage = async (id) => {
+    const imageToBeSaved = images.find((image) => image.id === id);
+    imageToBeSaved.saved = true;
+
+    try {
+      const res = await axios.post(`${API_URL}/images`, imageToBeSaved);
+      if (res.data?.inserted_id) {
+        setImages(
+          images.map((image) =>
+            image.id === id ? { ...image, saved: true } : image,
+          ),
+        );
+        toast.info(`Image ${imageToBeSaved.title.toUpperCase()} was saved`);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
   // console.log('images ', images);
 
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      <Container className="mt-4">
-        {images.length ? (
-          <Row xs={1} md={2} lg={3}>
-            {images.map((image, i) => (
-              <Col key={i} className="pb-3">
-                <ImageCard image={image} deleteImage={handleDeleteImage} />
-              </Col>
-            ))}
-          </Row>
-        ) : (
-          <Welcome />
-        )}
-      </Container>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Search
+            word={word}
+            setWord={setWord}
+            handleSubmit={handleSearchSubmit}
+          />
+          <Container className="mt-4">
+            {images.length ? (
+              <Row xs={1} md={2} lg={3}>
+                {images.map((image, i) => (
+                  <Col key={i} className="pb-3">
+                    <ImageCard
+                      image={image}
+                      deleteImage={handleDeleteImage}
+                      saveImage={handleSaveImage}
+                    />
+                  </Col>
+                ))}
+              </Row>
+            ) : (
+              <Welcome />
+            )}
+          </Container>
+        </>
+      )}
+      <ToastContainer position="bottom-right" />
     </div>
   );
 };
--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,22 +1,33 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Nav } from 'react-bootstrap';
 
-const ImageCard = ({ image, deleteImage }) => {
-  console.log('image ', image);
+const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || 'No author name';
+  const authorPortfolioURL = image.user?.portfolio_url;
+
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image.data.urls.small} />
+      <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
-        <Card.Title>{image.title.toUpperCase()}</Card.Title>
-        <Card.Text>{image.data.description || image.data.alt_description}</Card.Text>
-        <Button
-          variant="primary"
-          onClick={() => deleteImage(image.data.id)}
-        >
+        <Card.Title>{image.title?.toUpperCase()}</Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
           Delete
-        </Button>
+        </Button>{' '}
+        {!image.saved && (
+          <Button variant="secondary" onClick={() => saveImage(image.id)}>
+            Save
+          </Button>
+        )}
       </Card.Body>
+      <Card.Footer className="text-center text-muted">
+        {authorPortfolioURL && (
+          <Nav.Link href={authorPortfolioURL} target="_blank">
+            {authorName}
+          </Nav.Link>
+        )}
+        {!authorPortfolioURL && authorName}
+      </Card.Footer>
     </Card>
   );
 };
